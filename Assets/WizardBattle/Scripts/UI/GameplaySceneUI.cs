3be7846639639a5b549999ed34bdcf5d749d33c7
Andrei Maistruk <dok@ironictitle.com>
2024-07-24 04:47:01 +0400

    added basic ui and healthbar

--- Assets/WizardBattle/Scripts/UI/GameplaySceneUI.cs
+++ Assets/WizardBattle/Scripts/UI/GameplaySceneUI.cs
@@ -1,9 +1,17 @@
+using IT.CoreLib.Application;
 using IT.CoreLib.UI;
+using UnityEngine;
 
 namespace IT.WizardBattle.UI
 {
     public class GameplaySceneUI : SceneUIBase
     {
-        
+        [SerializeField] private UIHealthbarView _healthbar;
+
+        public override void Initialize(SceneBootstrap scene, ApplicationUIContainer appUIContainer)
+        {
+            base.Initialize(scene, appUIContainer);
+
+        }
     }
 }
---
+++ Assets/WizardBattle/Scripts/UI/UIHealthbarView.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace IT.WizardBattle.UI
+{
+    public class UIHealthbarView : MonoBehaviour
+    {
+        [SerializeField] private Image _fillBar;
+        [SerializeField] private float _fillingDuration = 0.3f;
+
+        private float _targetHealth = 1.0f;
+
+        private Coroutine _animationCoroutine;
+
+        /// <summary>
+        /// Set health amount
+        /// </summary>
+        /// <param name="health">from 0 to 1</param>
+        /// <param name="animated">animated</param>
+        public void SetHealthAmount(float health, bool animated = true)
+        {
+            if (health < 0.0f || health > 1.0f)
+                Debug.LogError($"[HEALTHBAR] Health is {health}, something gone wrong!");
+
+
+            if (_animationCoroutine != null)
+            {
+                StopCoroutine(_animationCoroutine);
+                _animationCoroutine = null;
+            }
+
+            _targetHealth = Mathf.Clamp01(health);
+
+            if (!animated)
+            {
+                SetValue(health);
+                return;
+            }
+
+            _animationCoroutine = StartCoroutine(FillHealthAnimated());
+            
+        }
+
+        private IEnumerator FillHealthAnimated()
+        {
+            float speed = Mathf.Abs(_fillBar.fillAmount - _targetHealth) / _fillingDuration;
+            float minValue = _fillBar.fillAmount;
+            float maxValue = _targetHealth;
+
+            if (_targetHealth < _fillBar.fillAmount)
+            {
+                speed *= -1.0f;
+                minValue = _targetHealth;
+                maxValue = _fillBar.fillAmount;
+            }
+            
+            while (_fillBar.fillAmount != _targetHealth)
+            {
+                float newValue = _fillBar.fillAmount + speed * Time.deltaTime;
+                _fillBar.fillAmount = Mathf.Clamp(newValue, minValue, maxValue);
+                yield return null;
+            }
+            
+        }
+
+        private void SetValue(float value)
+        {
+            _fillBar.fillAmount = value;
+        }
+
+        //TODO: Remove this test method
+        private void OnGUI()
+        {
+            if (GUI.Button(new Rect(10, 70, 50, 30), "Debug helath bar"))
+            {
+                SetHealthAmount(_fillBar.fillAmount - 0.1f);
+            }
+        }
+    }
+}
